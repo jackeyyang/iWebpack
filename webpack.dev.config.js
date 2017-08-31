4290af5ad8f3c14e3f0f7c80ec026f6c713346dd
jackyyang <jackey088@hotmail.com>
2017-08-31 15:31:57 +0800

    webpack config modified

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,14 +11,14 @@ module.exports = {
 		path: path.join(__dirname,'dist'),
 		filename: 'script/[name].js'
 	},	
-	devServer: {
-	  contentBase: path.join(__dirname,"dist"),
-	  port: 9000
-	},
 	plugins: [
 		new htmlWebpackPlugin({
 			template: './src/index.html',
 			filename: 'index.html'
 		})
-	]
+	],
+	devServer: {
+	  contentBase: path.join(__dirname,"dist"),
+	  port: 8088
+	}
 };
