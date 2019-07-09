b8db62bc863512e91196827adb9927f1b6ebdc60
jackyyang <jackey088@hotmail.com>
2019-07-09 16:26:27 +0800

    htmlWebpackPlugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -10,50 +10,67 @@ const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const pageFile = glob.sync('src/pages/**/view/**/*',{
     nodir: true
 });
-var entries = getEntry(pageFile);
+let config = {};
+config.entries = getEntryAndChunk(pageFile).entries;
+config.chunks = getEntryAndChunk(pageFile).chunks;
+config.plugins = [
+    // 抽取的公共库js
+    new webpack.optimize.CommonsChunkPlugin({
+        name: ['vendor/vendor'],
+        minChunks: Infinity
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: ['vendor/manifest'],
+        minChunks: Infinity
+    }),
+    /*new htmlWebpackPlugin({
+        template: './src/pages/index/view/index.html',
+        filename: 'index.html',
+        chunks: ["vendor/manifest","vendor/vendor","commonScript/common","pages/index/index"],
+        chunksSortMode: "manual" // manual根据chunks的位置手动排序
+    }),
+    new htmlWebpackPlugin({
+        template: './src/pages/login/view/login.html',
+        filename: 'login.html',
+        chunks: ["vendor/manifest","vendor/vendor","commonScript/common","pages/login/login"],
+        chunksSortMode: "manual"
+    }),*/
+    new HtmlWebpackHarddiskPlugin(),
+    new webpack.ProvidePlugin({
+        "$": "jquery",
+        "jQuery": "jquery",
+        "window.jQuery": "jquery"
+    }),
+    // 将css抽离生成
+    new ExtractTextPlugin({
+        filename: (getPath) => {
+            return getPath('[name].css').replace('commonScript','commonCss');
+        }
+    })
+];
+
+(config.chunks).forEach(function (item) {
+	let strArray = item.split('/');
+	let templateStr = strArray[strArray.length-1];
+
+	let htmlChunk = ["vendor/manifest","vendor/vendor","commonScript/common"]
+    htmlChunk.push(item);
+    config.plugins.push(new htmlWebpackPlugin({
+        template: './src/pages/'+templateStr+'/view/'+templateStr+'.html',
+        filename: templateStr+'.html',
+        chunks: htmlChunk,
+        chunksSortMode: "manual"
+    }));
+});
 
 module.exports = {
-	entry: entries,
+	entry: config.entries,
 	output: {
 		// path: path.join('F:\\Trunk2\\Trunk2Project\\javaWebTest\\WebContent','dist'),
 		path: path.join(__dirname,'dist'),
 		filename: '[name].[chunkhash:8].js'
 	},
-	plugins: [
-		// 抽取的公共库js
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['vendor/vendor'],
-            minChunks: Infinity
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['vendor/manifest'],
-            minChunks: Infinity
-        }),
-		new htmlWebpackPlugin({
-			template: './src/pages/index/view/index.html',
-			filename: 'index.html',
-			chunks: ["vendor/manifest","vendor/vendor","commonScript/common","pages/index/index"],
-			chunksSortMode: "manual" // manual根据chunks的位置手动排序
-		}),
-		new htmlWebpackPlugin({
-			template: './src/pages/login/view/login.html',
-			filename: 'login.html',
-			chunks: ["vendor/manifest","vendor/vendor","commonScript/common","pages/login/login"],
-			chunksSortMode: "manual"
-		}),
-		new HtmlWebpackHarddiskPlugin(),
-        new webpack.ProvidePlugin({
-            "$": "jquery",
-            "jQuery": "jquery",
-            "window.jQuery": "jquery"
-        }),
-    	// 将css抽离生成
-    	new ExtractTextPlugin({
-    		filename: (getPath) => {
-    			return getPath('[name].css').replace('commonScript','commonCss');
-    		}
-    	})
-	],
+	plugins: config.plugins,
 	module:{
 		rules:[
 			{
@@ -134,13 +151,18 @@ module.exports = {
 };
 
 
-function getEntry(pageFile) {
-    var entries = {};
+function getEntryAndChunk(pageFile) {
+	let backObj = {};
+    let entries = {};
+    let chunks = [];
     entries["vendor/vendor"] =  Object.keys(packagejson.dependencies); //获取生产环境依赖的库
     entries["commonScript/common"]= './src/commonScript/common.js';
 
     pageFile.forEach(function (item) {
         entries[(item.replace('src/','').replace('/view','')).replace(/\.html$/, '')] = (item.replace('src/','./src/').replace(/\.html$/, '')).replace('/view','/script');
+        chunks.push((item.replace('src/','').replace('/view','')).replace(/\.html$/, ''));
     });
-    return entries;
+    backObj["entries"] = entries;
+    backObj["chunks"] = chunks;
+    return backObj;
 }
